72889213bc49e94152ce4080bb4a08f692a37c03
vm22 <vm22@pepsico.com>
2019-07-17 16:18:02 +0000

    add parent_dir example

---
+++ test/parent_dir_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 16:12:54 2019
+
+@author: dxuser22
+"""
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import docstring_example
+
+def test_parent_dir():
+    docstring_example.func1()
+
+if __name__ == '__main__': 
+    docstring_example.func1()
+    print(os.path.abspath(__file__))
+    print(os.path.dirname(os.path.abspath(__file__)))
+    print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
