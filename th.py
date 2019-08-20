b15ebb3cc8db6271b6561850f731f21e9738b268
vm22 <vm22@pepsico.com>
2019-08-20 21:46:13 +0000

    add ev

--- th.py
+++ th.py
@@ -12,6 +12,9 @@ import numpy.testing as npt
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
+rake_rate = 0.1
+n = 1 # players against
+
 def draw(outs, denom, succeed_flag=True):
     if succeed_flag:
         return outs/denom
@@ -42,18 +45,51 @@ def draw_pt(outs, denom=46):
     return draw(outs, denom)
 
 
-def break_even_prob(pot, r):
-    return r/(pot + r * 2)
+def break_even_prob(pot, r,
+                    n = 1, rake_rate=0):
+    if rake_rate>0:
+        tmp = r/((pot + r * n)*(1-rake_rate) + r )
+    else:
+        tmp = r/(pot + r * (n+1))
+    return tmp
 
 
 def format_outs(outs):
     d1, d2 = draw_pf(outs) 
     dpt = draw_pt(outs)
-    return (outs, d1+d2, dpt)
+    return (outs, d1+d2, be_prob_to_r(d1+d2), dpt, be_prob_to_r(dpt))
 
 
-def be_prob_to_c(prob):
-    return prob/(1-2*prob)
+def be_prob_to_r(prob):
+    if prob < 0.5:
+        tmp = prob/(1-2*prob)
+    else:
+        tmp = -1
+    return tmp
+
+
+def prob_to_commit(prob, n = 1):
+    r = be_prob_to_r(prob)
+    pot_new = 1 + r * (n+1)
+    r_t = be_prob_to_r(prob) * pot_new
+    return r + r_t
+
+
+def call_ev(r, prob,
+            n=1, rake_rate=0):
+    ev = (1 + r*n) * (1-rake_rate) * prob - r * (1-prob)
+    return ev   
+    
+
+def raise_ev(r, my_prob,
+             n=1, rake_rate=0, fold_rate = 0.5):
+    keep = 1 - rake_rate
+    fold_pct = (1-my_prob) * fold_rate
+    ev = 1 * keep * fold_pct + \
+    (1 + r*n) * keep * r + \
+    (-r) * (1 - my_prob - fold_pct)
+    return ev
+
 
 pot = 40
 
@@ -81,4 +117,4 @@ ev1 = (pot + r1) * draw(outs, denom) + (-r1) * (1-draw(outs, denom))
 list_outs = list(range(5, 16))
 list_pf = list(map(format_outs, list_outs))
 
-df = pd.DataFrame(list_pf, columns=['o', 'pf', 'pt'])
+df = pd.DataFrame(list_pf, columns=['o', 'pf', 'r_f', 'pt', 'r_t'])
