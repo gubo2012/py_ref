b6f89ee7f8dadedf29eb09d5063da8b9afbb8417
vm22 <vm22@pepsico.com>
2019-10-22 21:16:32 +0000

    add xgb classifier

--- shift_and_rolling.py
+++ shift_and_rolling.py
@@ -10,7 +10,7 @@ import numpy as np
 
 import math
 
-N = 100
+N = 200
 scale = 10
 x = np.random.rand(N, 2)
 
@@ -40,3 +40,39 @@ df = pd.merge(df, df_rolling, left_index=True, right_index=True, how = 'left')
 
 
 df.plot(y = ['y1', 'y1_s', 'y1_rl'])
+
+
+#import matplotlib.pyplot as plt
+#
+#df_corr = df.corr()
+#plt.matshow(df.corr())
+#plt.show()
+
+df_concise = df[['x1', 'x2', 'n', 'y1', 'y1_s', 'y1_rl']].copy()
+
+df_concise['y1_1'] = df_concise['y1'] - df_concise['y1'].shift(1)
+df_concise = df_concise[df_concise.y1_1 == df_concise.y1_1]
+df_concise['y'] = df_concise.y1_1.apply(lambda x:1*(x>0))
+
+y = df_concise['y'].values
+X = df_concise[['x1', 'x2', 'n', 'y1_s', 'y1_rl', 'y1_1']].values
+
+
+# split data into train and test sets
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+seed = 7
+test_size = 0.25
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
+
+model = XGBClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+#predictions = [round(value) for value in y_pred]
+
+# evaluate predictions
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
